812425461a57989723b13be2a43b85a42078a4ca
Fabio Cavazzini <fabio.cavazzini@iticopernico.it>
2024-01-05 18:39:15 +0100

    Update all code

--- JavaScript/JavaScript.js
+++ JavaScript/JavaScript.js
@@ -41,9 +41,11 @@ function showSlides(n) {
 }
 
 //FORM
+var controllo = 0;
+
 function Richiedi() {
-  //Impedisce invio form facendo svolgere la funzione senza interruzzioni
-  event.preventDefault();
+
+  controllo = 1;
 
   document.getElementById("p_tickets").innerHTML="Biglietti richiesti";
 
@@ -68,127 +70,183 @@ function campiBiglietto(number) {
   //creazione div per singolo biglietto
   var biglietto = document.createElement("div");
 
+  //Creazione nome biglietto
+  var testo = document.createTextNode("Biglietto numero " + number + " ");
+
   //creazione input per Nome
   var nome = document.createElement("input");
   nome.setAttribute("name", "nome");
   nome.setAttribute("class", "nome");
   nome.setAttribute("type", "text");
-  nome.setAttribute("placeholder", "Nome persona n° " + number);
+  nome.setAttribute("placeholder", "Nome");
 
   //creazione input per Cognome
   var cognome = document.createElement("input");
   cognome.setAttribute("name", "cognome");
   cognome.setAttribute("class", "cognome");
   cognome.setAttribute("type", "text");
-  cognome.setAttribute("placeholder", "Cognome persona n° " + number);
+  cognome.setAttribute("placeholder", "Cognome");
+
+  //creazione scelta tipologia documento
+  var documento = document.createElement("select");
+  documento.setAttribute("name", "documento");
+  documento.setAttribute("class", "documento");
+
+  //creazione tag OPTION per la scelta del documento
+  var s0 = document.createElement("OPTION");
+  var d0 = document.createTextNode("Selezionare documento");
+  s0.appendChild(d0);
+  documento.appendChild(s0);
+
+  var s1 = document.createElement("OPTION");
+  var d1 = document.createTextNode("Carta di Identità");
+  s1.appendChild(d1);
+  documento.appendChild(s1);
+
+  var s2 = document.createElement("OPTION");
+  var d2 = document.createTextNode("Passaporto");
+  s2.appendChild(d2);
+  documento.appendChild(s2);
 
   //creazione input per età
-  var eta = document.createElement("input");
-  eta.setAttribute("name", "eta");
-  eta.setAttribute("class", "eta");
-  eta.setAttribute("type", "text");
-  eta.setAttribute("placeholder", "Età persona n° " + number);
+  var nDoc = document.createElement("input");
+  nDoc.setAttribute("name", "nDoc");
+  nDoc.setAttribute("class", "nDoc");
+  nDoc.setAttribute("type", "text");
+  nDoc.setAttribute("placeholder", "Numero documento");
 
   //aggiunta al div di ogni biglietto dei campi di input
+  biglietto.appendChild(testo);
   biglietto.appendChild(nome);
   biglietto.appendChild(cognome);
-  biglietto.appendChild(eta);
+  biglietto.appendChild(documento);
+  biglietto.appendChild(nDoc);
 
   return biglietto;
 }
 
 function Ordine() {
-  //Impedisce invio form facendo svolgere la funzione senza interruzzioni
-  event.preventDefault();
 
-  var nBiglietti = document.getElementById("number").value;
-
-  var prezzo;
-  var destinazione;
-  var scontrino;
-
-  //fare in base alla scelta
-  switch(document.getElementById("scelta").value)
+  if(controllo==1)
   {
-    //Dubai
-    case 1: {
-      prezzo = 277;
-      destinazione = "DUBAI";
-      break;
-    }
+    var nBiglietti = document.getElementById("number").value;
 
-    //NewYork
-    case 2: {
-      prezzo = 220;
-      destinazione = "NEWYORK";
-      break;
-    }
+    var prezzo;
+    var destinazione;
+    var scontrino;
 
-    //Londra
-    case 3: {
-      prezzo = 138;
-      destinazione = "LONDRA";
-      break;
-    }
+    var scelta = document.querySelector('input[name="sc"]:checked');
+    var valoreScelta = scelta ? scelta.value : null;
 
-    //Parigi
-    case 4: {
-      prezzo = 143;
-      destinazione = "PARIGI";
-      break;
-    }
+    if (valoreScelta !== null)
+    {
+      switch (parseInt(valoreScelta))
+      {
+        //Dubai
+        case 1: {
+          prezzo = 277;
+          destinazione = "DUBAI";
+          break;
+        }
 
-    //Milano
-    case 5: {
-      prezzo = 61;
-      destinazione = "MILANO";
-      break;
-    }
+        //NewYork
+        case 2: {
+          prezzo = 220;
+          destinazione = "NEWYORK";
+          break;
+        }
 
-    //Venezia
-    case 6: {
-      prezzo = 211;
-      destinazione = "VENEZIA";
-      break;
-    }
+        //Londra
+        case 3: {
+          prezzo = 138;
+          destinazione = "LONDRA";
+          break;
+        }
 
-    //Firenze
-    case 7: {
-      prezzo = 36;
-      destinazione = "FIRENZE";
-      break;
-    }
+        //Parigi
+        case 4: {
+          prezzo = 143;
+          destinazione = "PARIGI";
+          break;
+        }
 
-    //Roma
-    case 8: {
-      prezzo = 53;
-      destinazione = "ROMA";
-      break;
-    }
-  }
+        //Milano
+        case 5: {
+          prezzo = 61;
+          destinazione = "MILANO";
+          break;
+        }
 
-  var prezzoTot = nBiglietti * prezzo;
+        //Venezia
+        case 6: {
+          prezzo = 211;
+          destinazione = "VENEZIA";
+          break;
+        }
+
+        //Firenze
+        case 7: {
+          prezzo = 36;
+          destinazione = "FIRENZE";
+          break;
+        }
+
+        //Roma
+        case 8: {
+          prezzo = 53;
+          destinazione = "ROMA";
+          break;
+        }
+
+        default: {
+          alert("errore");
+        }
+      }
+
+      var prezzoTot = nBiglietti * prezzo;
+
+      //generazione posto
+      var nPosto = Math.floor(Math.random() * 50) + 1;
+      var lPosto = String.fromCharCode(65 + Math.floor(Math.random() * 26));
+      
+      scontrino = "Volo richiesto:\n";
+      scontrino += "Da: BOLOGNA\n";
+      scontrino += "A: " + destinazione + "\n";
+      scontrino += "N° biglietti richiesti: " + nBiglietti + "\n";
+      scontrino += "\n";
+      scontrino += "----------BIGLIETTI----------\n";
+        
+      for (var i = 0; i < nBiglietti; i++) 
+      {
+        var nome = document.getElementsByName("nome")[i].value;
+        var cognome = document.getElementsByName("cognome")[i].value;
+        var documento = document.getElementsByName("documento")[i].value;
+        var nDoc = document.getElementsByName("nDoc")[i].value;
+        
+        scontrino += "PASSEGGERO N° " + (i + 1) + ":\n";
+        scontrino += "Nome: " + nome + "\n";
+        scontrino += "Cognome: " + cognome + "\n";
+        scontrino += "Documento: " + documento + "\n";
+        scontrino += "Numero documento: " + nDoc + "\n";
+        scontrino += "Posto numero: " + lPosto + nPosto + "\n";
+        scontrino += "--------------------------------------\n";
+
+        nPosto++;
+      }
   
-  scontrino = "Volo richiesto:\n";
-  scontrino += "Da: BOLOGNA\n";
-  scontrino += "A: " + destinazione + "\n";
-  scontrino += "N° biglietti richiesti: " + nBiglietti + "\n";
-  scontrino += "\n";
-  scontrino += "----------BIGLIETTI----------\n";
-    
-  for (var i = 0; i < nBiglietti; i++) 
-  {
-    var nome = document.getElementsByName("Nome")[i].value;
-    var cognome = document.getElementsByName("Cognome")[i].value;
-    var eta = document.getElementsByName("Eta")[i].value;
-    
-    scontrino += "PASSEGGERO N° " + (i + 1) + ":\n";
-    scontrino += "Nome: " + nome + "\n";
-    scontrino += "Cognome: " + cognome + "\n";
-    scontrino += "Età: " + eta + "\n";
-    scontrino += "--------------------------------------\n";
+      scontrino += "Prezzo singolo biglietto: " + prezzo + " €\n";
+      scontrino += "Prezzo per " + nBiglietti + " persone: " + prezzoTot + " €";
+  
+      console.log(scontrino);
+      alert(scontrino);
+    }
+    else
+    {
+      alert("Selezionare una destinazione");
+    }
   }
-  scontrino += "\nIl prezzo totale è: " + prezzoTot + " €";
-
-  alert(scontrino);
+  else
+  {
+    alert("Richiedere prima i biglietti");
+  }
 }
